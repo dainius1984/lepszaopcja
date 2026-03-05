e86f80a69e68ed6cfb054c2d554bdaad3cc0c86c
dainius1984 <marcinarturchmielnicki@gmail.com>
2026-03-05 20:39:36 +0100

    Initial React + Tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-slate-900">
+      <h1 className="text-4xl font-bold text-white">
+        Hello from Tailwind + React
+      </h1>
+    </div>
+  );
+}
+export default App;
